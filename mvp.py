20f53dd248414ab9b54bb79e7545fc56b1a3583b
Rishabh Ganesh <70454763+rishabhg19@users.noreply.github.com>
2022-05-29 15:08:47 -0400

    Update mvp.py

--- mvp.py
+++ mvp.py
@@ -19,8 +19,7 @@ def app():
 	with method:
 		st.header("Summarization method")
 		st.markdown("We used the summarizer from the pipeline imported through the transformers library to generate summaries for each article. After looking at open-source projects on GitHub, we thought this was a great abstractive text summarization model. The below line with the specified parameters generates summaries in the ballpark of 280 characters.")
-		st.text("summary = summarizer(text, max_length=55, min_length = 15, do_sample = False)[0][\'summary_text\']")
+		st.text("summary = summarizer(text, max_length=55, min_length = 15)[0][\'summary_text\']")
 		st.markdown("fit_characters() is a function we wrote to ensure that the summaries fit the 280 character maximum, while also ensuring that they end on a complete sentence.")
-		st.text("fit_characters(summ):\n\ttool = language_tool_python.LanguageTool('en-US')\n\tif (summ[len(summ)-1] == '.' and len(summ) <= 280):\n\t\treturn tool.correct(summ)\n\tsumm = summ[:280]\n\tsentences = summ.split(\".\")\n\tsentences.pop()\n\tnewsum = \'.\'.join(sentences)\n\tif newsum[-1] == \' \':\n\t\tnewsum = newsum[:(len(newsum)-1)]+\".\"\n\treturn tool.correct(newsum)")
-
+		st.text("fit_characters(summ):\n\ttool = language_tool_python.LanguageTool('en-US', config={ 'disabledRuleIds': 'MORFOLOGIK_RULE_EN_US'})\n\tif (summ[len(summ)-1] == '.' and len(summ) <= 280):\n\t\treturn tool.correct(summ)\n\tsumm = summ[:280]\n\tsentences = summ.split(\".\")\n\tsentences.pop()\n\tnewsum = \'.\'.join(sentences)\n\tif newsum[-1] == \' \':\n\t\tnewsum = newsum[:(len(newsum)-1)]+\".\"\n\treturn tool.correct(newsum)")
 	
