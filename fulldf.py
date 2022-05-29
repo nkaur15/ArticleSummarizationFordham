83a2afce72d354c728346ebd38244447da5cfd00
Rishabh Ganesh <70454763+rishabhg19@users.noreply.github.com>
2022-05-29 15:07:49 -0400

    Update fulldf.py

--- fulldf.py
+++ fulldf.py
@@ -7,8 +7,9 @@ def app():
 	with summs:
 		sumdf = pd.DataFrame()
 		st.header("Dataframe with all summaries and recommendations")
-		articledf = pd.read_csv('data/newest.csv')
+		articledf = pd.read_csv('data/bigdf.csv')
 		sumdf['ARTICLE'] = [articledf['Text'][i] for i in range(730)]
 		sumdf['SUMMARY'] = [articledf['Summary'][i] for i in range(730)]
 		sumdf['RECOMMENDATION'] = [articledf['Text'][articledf['Recommendation_Article_Num_Renumbered'][i]] for i in range(730)]
+		#sumdf['ARTICLE'] = articledf['Text']
 		st.write(sumdf)
